bf3dd34f423fff5a3f6577a4eab29964f8042a44
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-06-29 22:38:22 +0000

    Setup project

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+//pages
+import Home from '../pages/home/Home'
+
+//styles
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
