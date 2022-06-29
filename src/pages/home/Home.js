0f7e9e4990907e90f11c73d23d3aad5284175a1d
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-06-29 23:43:57 +0000

    define states and create modal for about us

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Home = () => {
+  const [ isAbout, setIsAbout ] = useState(true)
+  const [ isFaq, setIsFaq ] = useState(false)
+  const [ isContact, setIsContact ] = useState(false)
+
   return (
     <div className='home'>
+      {isAbout && (
+        <div className='modal-bg'>
+          <div className='modal'>
+            <h2 className='modal-title'>ABOUT US</h2>
+            <div className='modal-content'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat.</p> 
+              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse 
+                cillum dolore eu fugiat nulla pariatur.</p>
+              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                officia deserunt mollit anim id est laborum.</p>
+              <button 
+                className='modal-close'
+                onClick={() => setIsAbout(false)}>CLOSE</button>
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className='home-logo'>
         <img src="" alt="" />
         <p>LOGO</p>
@@ -10,7 +35,9 @@ const Home = () => {
       </div>
 
       <div className='home-content'>
-        <div className='home-content-card'>
+        <div 
+          className='home-content-card' 
+          onClick={() => setIsAbout(true)}>
           <p>&gt; ABOUT US</p>
         </div>
         <div className='home-content-card'>
