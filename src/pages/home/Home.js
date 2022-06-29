453dfbc81a3d1e852640beecdb893a961d104fe1
Gloria Sekyere <gloriasekyere15@gmail.com>
2022-06-29 23:12:45 +0000

    create and styles Home component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='home-logo'>
+        <img src="" alt="" />
+        <p>LOGO</p>
+        <h1>EDEM'S FOODS <span>Freshness overload all year round</span></h1>
+      </div>
+
+      <div className='home-content'>
+        <div className='home-content-card'>
+          <p>&gt; ABOUT US</p>
+        </div>
+        <div className='home-content-card'>
+          <p>&gt; FREQUENTLY ASKED</p>
+        </div>
+        <div className='home-content-card'>
+          <p>&gt; CONTACT</p>
+        </div>
+      </div>
+
+      <div className='home-shop'>
+        <a href="#">SHOP</a>
+      </div>
+    </div>
   )
 }
 
